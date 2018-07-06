25b28ff928aa7e0befc6bde39aea474a74ea5ad3
Ronald Lobo <rlb@akc.org>
2018-07-06 16:26:28 -0600

    comentar

--- API/Servicios/email.php
+++ API/Servicios/email.php
@@ -547,11 +547,28 @@ class EmailServicios {
                     </center>
                 </body>
             </html>';
-            echo $to;
-            echo $subject;
-            echo $message;
-            echo $headers;
+            // echo $to;
+            // echo $subject;
+            // echo $message;
+            // echo $headers;
         mail($to,$subject,$message,$headers);
+  //       $email = new \SendGrid\Mail\Mail(); 
+		// $email->setFrom("test@example.com", "Example User");
+		// $email->setSubject("Sending with SendGrid is Fun");
+		// $email->addTo("test@example.com", "Example User");
+		// $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+		// $email->addContent(
+		//     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+		// );
+		// $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+		// try {
+		//     $response = $sendgrid->send($email);
+		//     print $response->statusCode() . "\n";
+		//     print_r($response->headers());
+		//     print $response->body() . "\n";
+		// } catch (Exception $e) {
+		//     echo 'Caught exception: ',  $e->getMessage(), "\n";
+		// }
         return $usuarios;
     }
     

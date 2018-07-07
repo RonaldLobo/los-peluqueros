d7e0bcdee8b8d48c90a554af39ebbf394f04a703
Ronald Lobo <rlb@akc.org>
2018-07-07 08:24:29 -0600

    arreglando emails

--- API/Servicios/email.php
+++ API/Servicios/email.php
@@ -18,8 +18,8 @@ class EmailServicios {
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
         $headers .= 'From: <reservas@lospeluqueros.com>' . "\r\n";
         $to = $usuarios['correo'];
-        echo 'correo';
-        echo $to;
+        // echo 'correo';
+        // echo $to;
         $subject="Confirmación de Cita en ".$usuarios['barberia'];
         $message='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml">
@@ -552,45 +552,47 @@ class EmailServicios {
             // echo $message;
             // echo $headers;
         // mail($to,$subject,$message,$headers);
-        $sendgrid_apikey = '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'
-        // $sendgrid_apikey = getenv('YOUR_SENDGRID_APIKEY');
-		// $sendgrid = new SendGrid($sendgrid_apikey);
-		$url = 'https://api.sendgrid.com/';
-		$pass = $sendgrid_apikey;
-		$template_id = '<your_template_id>';
-		$js = array(
-		  'sub' => array(':name' => array('Elmer')),
-		  'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
-		);
 
-		$params = array(
-		    'to'        => "ronald.lb2@gmail.com",
-		    'toname'    => "Ronald Lobo",
-		    'from'      => "reservas@lospeluqueros.com",
-		    'fromname'  => "Reservas Los Peluqueros",
-		    'subject'   => "Lista su reserva con los peluqueros",
-		    'text'      => "Lista la reserva",
-		    'html'      => "<strong>Soy el contenido de la reserva en html</strong>",
-		  );
+            
+  //       $sendgrid_apikey = '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'
+  //       // $sendgrid_apikey = getenv('YOUR_SENDGRID_APIKEY');
+		// // $sendgrid = new SendGrid($sendgrid_apikey);
+		// $url = 'https://api.sendgrid.com/';
+		// $pass = $sendgrid_apikey;
+		// $template_id = '<your_template_id>';
+		// $js = array(
+		//   'sub' => array(':name' => array('Elmer')),
+		//   'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
+		// );
 
-		$request =  $url.'api/mail.send.json';
+		// $params = array(
+		//     'to'        => "ronald.lb2@gmail.com",
+		//     'toname'    => "Ronald Lobo",
+		//     'from'      => "reservas@lospeluqueros.com",
+		//     'fromname'  => "Reservas Los Peluqueros",
+		//     'subject'   => "Lista su reserva con los peluqueros",
+		//     'text'      => "Lista la reserva",
+		//     'html'      => "<strong>Soy el contenido de la reserva en html</strong>",
+		//   );
 
-		// Generate curl request
-		$session = curl_init($request);
-		// Tell PHP not to use SSLv3 (instead opting for TLS)
-		curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-		curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
-		// Tell curl to use HTTP POST
-		curl_setopt ($session, CURLOPT_POST, true);
-		// Tell curl that this is the body of the POST
-		curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-		// Tell curl not to return headers, but do return the response
-		curl_setopt($session, CURLOPT_HEADER, false);
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+		// $request =  $url.'api/mail.send.json';
 
-		// obtain response
-		$response = curl_exec($session);
-		curl_close($session);
+		// // Generate curl request
+		// $session = curl_init($request);
+		// // Tell PHP not to use SSLv3 (instead opting for TLS)
+		// curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+		// curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
+		// // Tell curl to use HTTP POST
+		// curl_setopt ($session, CURLOPT_POST, true);
+		// // Tell curl that this is the body of the POST
+		// curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+		// // Tell curl not to return headers, but do return the response
+		// curl_setopt($session, CURLOPT_HEADER, false);
+		// curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+		// // obtain response
+		// $response = curl_exec($session);
+		// curl_close($session);
         return $usuarios;
     }
     

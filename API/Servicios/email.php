2e2fa86add44b6a9251489defaa168bae1947fe9
Ronald Lobo <rlb@akc.org>
2018-07-07 08:18:51 -0600

    prueba mail

--- API/Servicios/email.php
+++ API/Servicios/email.php
@@ -551,24 +551,46 @@ class EmailServicios {
             // echo $subject;
             // echo $message;
             // echo $headers;
-        mail($to,$subject,$message,$headers);
-  //       $email = new \SendGrid\Mail\Mail(); 
-		// $email->setFrom("test@example.com", "Example User");
-		// $email->setSubject("Sending with SendGrid is Fun");
-		// $email->addTo("test@example.com", "Example User");
-		// $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-		// $email->addContent(
-		//     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-		// );
-		// $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-		// try {
-		//     $response = $sendgrid->send($email);
-		//     print $response->statusCode() . "\n";
-		//     print_r($response->headers());
-		//     print $response->body() . "\n";
-		// } catch (Exception $e) {
-		//     echo 'Caught exception: ',  $e->getMessage(), "\n";
-		// }
+        // mail($to,$subject,$message,$headers);
+        $sendgrid_apikey = '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'
+        // $sendgrid_apikey = getenv('YOUR_SENDGRID_APIKEY');
+		// $sendgrid = new SendGrid($sendgrid_apikey);
+		$url = 'https://api.sendgrid.com/';
+		$pass = $sendgrid_apikey;
+		$template_id = '<your_template_id>';
+		$js = array(
+		  'sub' => array(':name' => array('Elmer')),
+		  'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
+		);
+
+		$params = array(
+		    'to'        => "ronald.lb2@gmail.com",
+		    'toname'    => "Ronald Lobo",
+		    'from'      => "reservas@lospeluqueros.com",
+		    'fromname'  => "Reservas Los Peluqueros",
+		    'subject'   => "Lista su reserva con los peluqueros",
+		    'text'      => "Lista la reserva",
+		    'html'      => "<strong>Soy el contenido de la reserva en html</strong>",
+		  );
+
+		$request =  $url.'api/mail.send.json';
+
+		// Generate curl request
+		$session = curl_init($request);
+		// Tell PHP not to use SSLv3 (instead opting for TLS)
+		curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+		curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
+		// Tell curl to use HTTP POST
+		curl_setopt ($session, CURLOPT_POST, true);
+		// Tell curl that this is the body of the POST
+		curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+		// Tell curl not to return headers, but do return the response
+		curl_setopt($session, CURLOPT_HEADER, false);
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+		// obtain response
+		$response = curl_exec($session);
+		curl_close($session);
         return $usuarios;
     }
     

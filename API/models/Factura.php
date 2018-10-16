e684b999166bed046c4452a4963ed7dc85acba24
Ronald Lobo <rlobo@isthmusit.com>
2018-10-15 19:00:05 -0600

    actualizaciones factura

--- API/models/Factura.php
+++ API/models/Factura.php
@@ -5,7 +5,7 @@ class Factura {
     public $fecha ='';    
     public $id =0;
     public $estado=0;
-    public $idCliente=0;
+    public $idCliente;
     public $idCreadoPor=0;
     public $idSucursal=0;
     public $total=0;

24ea461e75205aca7e8a73d04dd49b599ca76fdb
Ronald Lobo <rlb@akc.org>
2019-03-23 06:12:26 -0600

    job

---
+++ API/facturas.php
@@ -0,0 +1,3 @@
+<?php
+	error_log("Existe el job de facturas q corre cada 10 mins", 0);
+?>
---
+++ API/facturasjob.php
@@ -0,0 +1,3 @@
+<?php
+	$payload = file_get_contents('http://los-peluqueros.herokuapp.com/API/facturas.php');
+?>
